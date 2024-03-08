1f27e76ee3e7bea9e6dd09c5f0933c0462d6f98b
Sri Ujjwal Reddy Beereddy <113922053+sbeeredd04@users.noreply.github.com>
2024-03-08 09:06:22 +0000

    blank page

---
+++ sri-s_portfolio/src/AboutMe.js
@@ -0,0 +1,12 @@
+
+
+export default function AboutMe() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>About Me</h1>
+                <p>My name is Sri and I am a software engineer.</p>
+            </header>
+        </div>
+    );
+}
