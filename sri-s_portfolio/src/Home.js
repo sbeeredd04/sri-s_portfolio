50676089a080a6b7ecab224adb34c7e7e705e16d
Sri Ujjwal Reddy Beereddy <113922053+sbeeredd04@users.noreply.github.com>
2024-03-08 13:37:59 +0000

    Complete HOME page

--- sri-s_portfolio/src/Home.js
+++ sri-s_portfolio/src/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import Typed from 'typed.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faGithub, faInstagram, faDiscord, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faGithub, faInstagram, faDiscord, faSpotify, faRightFromBracket } from '@fortawesome/free-brands-svg-icons';
 import logo from '/workspaces/sri-s_portfolio/sri-s_portfolio/src/Images/logo.jpg';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     useEffect(() => {
@@ -39,23 +40,23 @@ export default function Home() {
                     <p>I'm a sophomore at Arizona State University<br />currently pursuing my Bachelors in Computer Science.</p>
                     <div className="home-sci">
 
-                        <a href="https://www.linkedin.com/in/sri-ujjwal-reddy-8a52b42b9/" target="blank" title="LinkedIn" className="linkedinCircle" style={{ '--i': 7,color:'#00a0dc', borderColor: '#00a0dc', backgroundColor: '00a0dc' }}>
+                        <a href="https://www.linkedin.com/in/sri-ujjwal-reddy-8a52b42b9/" target="blank" title="LinkedIn" className="linkedinCircle" style={{ '--i': 7, color: '#00a0dc', borderColor: '#00a0dc', backgroundColor: '00a0dc' }}>
                             <FontAwesomeIcon icon={faLinkedinIn} />
                         </a>
 
-                        <a href="https://github.com/sbeeredd04" target="blank" title="GitHub" className="githubCircle" style={{ '--i': 8, color: '#fafafa', borderColor: '#fafafa',  backgroundColor: '#000' }}>
+                        <a href="https://github.com/sbeeredd04" target="blank" title="GitHub" className="githubCircle" style={{ '--i': 8, color: '#fafafa', borderColor: '#fafafa', backgroundColor: '#000' }}>
                             <FontAwesomeIcon className='github' icon={faGithub} />
                         </a>
 
-                        <a href="https://www.instagram.com/b.sriujjwal/?theme=dark" target="blank" title="Instagram" className ="InstagramCircle" style={{ '--i': 9, color:'#dd2a7b', borderColor: '#dd2a7b', backgroundColor:'#000' }}>
+                        <a href="https://www.instagram.com/b.sriujjwal/?theme=dark" target="blank" title="Instagram" className="InstagramCircle" style={{ '--i': 9, color: '#dd2a7b', borderColor: '#dd2a7b', backgroundColor: '#000' }}>
                             <FontAwesomeIcon className='instagram' icon={faInstagram} />
                         </a>
 
-                        <a href="https://discord.com/users/ace_vortx" target="blank " title="Discord" className = "discordCircle" style={{ '--i': 10, color:'#7289da', borderColor:'#7289da', backgroundColor:'#000' }}>
+                        <a href="https://discord.com/users/ace_vortx" target="blank " title="Discord" className="discordCircle" style={{ '--i': 10, color: '#7289da', borderColor: '#7289da', backgroundColor: '#000' }}>
                             <FontAwesomeIcon className='discord' icon={faDiscord} />
                         </a>
 
-                        <a href="https://open.spotify.com/user/31qr3j45nvoqp4lfh6vuabmlwguq?si=7236606e83ca497d" target="blank " title="Spotify" className = 'spotifyCircle' style={{ '--i': 11, color:'#1ed760', borderColor:'#1ed760', backgroundColor:'#000'}}>
+                        <a href="https://open.spotify.com/user/31qr3j45nvoqp4lfh6vuabmlwguq?si=7236606e83ca497d" target="blank " title="Spotify" className='spotifyCircle' style={{ '--i': 11, color: '#1ed760', borderColor: '#1ed760', backgroundColor: '#000' }}>
                             <FontAwesomeIcon className='spotify' icon={faSpotify} />
                         </a>
                     </div>
@@ -64,6 +65,14 @@ export default function Home() {
                 {/*image */}
                 <div className="home-img">
                     <img src={logo} alt="Logo" className='Logo' />
+                    <Link to="/aboutme" className="aboutmebutton">
+                        <button className="learn-more">
+                            <span className="circle" ariaHidden="true">
+                                <span className="icon arrow"></span>
+                            </span>
+                            <span className="button-text">About Me</span>
+                        </button>
+                    </Link>
                 </div>
 
             </section>
