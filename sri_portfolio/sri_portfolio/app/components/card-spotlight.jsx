fc2f4c1d521fd21e09a5cb9c98c98989e3a67ca3
sbeeredd04 <sbeeredd@asu.edu>
2024-10-24 18:31:22 -0700

    updated blog post with spotlight cards

---
+++ sri_portfolio/sri_portfolio/app/components/card-spotlight.jsx
@@ -0,0 +1,53 @@
+// card-spotlight.jsx
+import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
+import React, { useState } from "react";
+import { cn } from "../lib/utils";
+
+export const CardSpotlight = ({
+  children,
+  radius = 350,
+  color = "#262626",
+  className,
+  ...props
+}) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+  function handleMouseMove({ currentTarget, clientX, clientY }) {
+    let { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
+
+  return (
+    <div
+      className={cn(
+        "group/spotlight p-10 rounded-md relative border border-neutral-800 bg-black dark:border-neutral-800",
+        className
+      )}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      {...props}>
+      <motion.div
+        className="pointer-events-none absolute z-0 -inset-px rounded-md opacity-0 transition duration-300 group-hover/spotlight:opacity-100"
+        style={{
+          backgroundColor: color,
+          maskImage: useMotionTemplate`
+            radial-gradient(
+              ${radius}px circle at ${mouseX}px ${mouseY}px,
+              white,
+              transparent 80%
+            )
+          `,
+        }}>
+        {/* Remove CanvasRevealEffect for now */}
+      </motion.div>
+      {children}
+    </div>
+  );
+};
